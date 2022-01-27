ffca36b5b63d9ec94670c13b0214aeb399c278d3
karinamincane <98511790+karinamincane@users.noreply.github.com>
2022-01-27 08:50:10 +0200

    Deleted property added new method

--- bank_main.py
+++ bank_main.py
@@ -22,9 +22,9 @@ class Account:
         self.initial_balance = initial_balance
         self.timestamp = datetime.datetime.now()
 
-    @property #decorators
-    def account_number(self):
-        return self._account_number
+    
+    def print_transaction():
+        pass
 
 class Client:
     def __init__(self, name: str):
